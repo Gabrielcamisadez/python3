09a3597901d2a47622cb4e3495b2590d31027dc8
Gabriel Ferreira Andrade Cardoso <tr26530es@trf1.gov.br>
2024-10-21 16:31:36 -0300

    ok'

---
+++ second.py
@@ -0,0 +1,12 @@
+import random
+import sys
+
+
+names = ["Lebron", "Durant", "Curry", "Giannis", "Doncic"]
+print("Welcome!!")
+
+def main() -> None:
+    print(random.choice(names))
+    print(sys.argv)
+    
+main()    
