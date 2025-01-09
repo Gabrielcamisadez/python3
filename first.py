b0a6d9c375fd750cd4f1a470cbdf96910a514530
Gabriel Ferreira <gabriel448521@gmail.com>
2025-01-08 22:38:31 -0300

    Primeiro script com relevância ?

--- first.py
+++ first.py
@@ -1,2 +1,11 @@
-#
+import socket
+dominio = "dominio.com"
+nomes = ["www", "ftp", "mail", "pop", "smtp", "webmail"]
+
+for nome in nomes:
+    DNS = nome + "." + dominio
+    try:
+        print(DNS + ": " + socket.gethostbyname(DNS))
+    except socket.gaierror:
+        print(DNS + ": NOt find")
 
