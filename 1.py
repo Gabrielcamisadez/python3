2c2ac86c27a76de7dc09f336c8fb810721df5145
Gabriel Ferreira <gabriel448521@gmail.com>
2025-01-28 10:05:39 -0300

    .

--- 1.py
+++ 1.py
@@ -1,21 +1,17 @@
+from fastapi import FastAPI
 import requests
 from bs4 import BeautifulSoup
 import os
 import json
 
-#dictionary = {
-#
-#}
-#
-#r1 = requests.get("https://servicodados.ibge.gov.br/api/v3/agregados/1209/periodos/2022/variaveis/1000606?localidades=N1[all]&classificacao=58[92982]")
-#
-#data = r1.json()
-#
-#print(json.dumps(data, indent=3))
+app = FastAPI()
 
-link = "https://www.bcb.gov.br/estabilidadefinanceira/fechamentodolar"
+@app.get("/")
+def read_root():
+    
 
-r1 = requests.get(link)
+
+
 site = BeautifulSoup(r1.text, "html.parser")
 #print(site.prettify())
 
--- fast.py
+++ fast.py
@@ -1,23 +1,26 @@
 from fastapi import FastAPI 
 import pandas as pd
 from flask import jsonify
+import uvicorn
 
 
 app = FastAPI()
 
-domains = pd.read_excel("domains.xlsx")
+names = pd.read_csv('password_wordlist.txt')
 
 
 
 @app.get("/")
 def read_root():
-    js = domains.to_json()
+    js = names.to_json()
     return js
 
 @app.get("/ips")
 def get_ip():
-    tojson = domains.to_dict()
+    tojson = names.to_dict()
     return tojson
 def read_ips():
     return(jsonify(get_ip()))
 
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
